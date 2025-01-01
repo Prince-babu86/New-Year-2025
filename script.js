c06d53f4c2e7e3e589fe1acfcc8310de2baf7f93
Prince Babu <princebabu4495@gmail.com>
2025-01-01 16:40:00 +0530

    intail add

---
+++ script.js
@@ -0,0 +1,210 @@
+const tl = gsap.timeline();
+
+tl.from(".heads" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:2,
+    stragger:-1,
+    rotate:20,
+    scale:4.2
+})
+
+tl.to(".heads" , {
+   
+})
+
+tl.from("#new1" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+tl.from("#new2" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new3" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new4" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+tl.from("#new5" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new6" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new7" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new8" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new9" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+tl.from("#new10" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new11" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+
+tl.from("#new12" , {
+    x:15,
+    y:10,
+    opacity:0,
+    delay:0.3,
+    duration:0.3,
+    stragger:-1,
+    rotate:20,
+    scale:15.5
+})
+
+tl.from("#new13" , {
+    y:-1000,
+    duration:1,
+    delay:0.3,
+    opacity:0,
+    scale:5
+})
+
+tl.from("#new14" , {
+    y:-1000,
+    duration:1,
+    delay:0.3,
+    opacity:0,
+    scale:5
+})
+
+tl.from("#new15" , {
+    y:-1000,
+    duration:1,
+    delay:0.3,
+    opacity:0,
+    scale:5
+
+})
+
+tl.from("#new16" , {
+    y:-1000,
+    duration:1,
+    delay:0.3,
+    opacity:0,
+    scale:5
+
+})
+
+tl.to("#new16" , {
+    y:-1000,
+    duration:1,
+    delay:0.3,
+    opacity:0,
+    scale:5,
+    display:"none"
+})
+
+
+tl.from("#new17" , {
+    y:-1000,
+    duration:1,
+    delay:0.3,
+    opacity:0,
+    x:-10,
+    scale:5
+
+})
